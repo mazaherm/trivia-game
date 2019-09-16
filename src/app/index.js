fb10e360b48f213c158edaa9002bba7cf926c497
Mazaher Muraj <mmuraj.313@gmail.com>
2019-09-16 22:44:23 +0100

    onclick fetch data using axios

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,18 @@
-import '../styles/main.scss'
-console.log('It works')
+import axios from 'axios'
+
+let difficulty = 'easy'
+
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', () => {
+  axios.get(`https://opentdb.com/api.php?amount=1&difficulty=${difficulty}`)
+    .then(response => {
+    console.log(response.data.results[0])
+  })
+    .catch(error => {
+    console.log(error)
+  })
+})
+
+
+
